11c621ce57d55fdae8f7278f34d0b6b67709bab3
vsblemos <victorserralemos@gmail.com>
2024-02-20 16:04:31 -0300

    update docker file

--- database/db.go
+++ database/db.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"log"
-
+	"os"
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,8 +13,13 @@ var (
 	err error
 )
 
-func ConectaComBancoDeDados() {
-	stringDeConexao := "host=localhost user=postgres password=root dbname=root port=5432 sslmode=disable"
+	func ConectaComBancoDeDados() {
+		host := os.Getenv("HOST")
+		user := os.Getenv("USER")
+		password := os.Getenv("PASSWORD")
+		dbname := os.Getenv("DBNAME")
+		port := os.Getenv("DBPORT")
+		stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
