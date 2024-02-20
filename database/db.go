e665cf7d3c7695eda0e3651b9b595d518c42458b
serralemosdev <152344764+serralemosdev@users.noreply.github.com>
2024-02-20 16:42:55 -0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -14,12 +14,31 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-    host := os.Getenv("HOST")
-    user := os.Getenv("USER")
-    password := os.Getenv("PASSWORD")
-    dbname := os.Getenv("DBNAME")
-    port := os.Getenv("DBPORT")
-    stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
+    package database
+
+import (
+	"log"
+	"os"
+	"github.com/guilhermeonrails/api-go-gin/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	
+	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("USER")+" password="+os.Getenv("PASSWORD")+" dbname="+os.Getenv("DBNAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+
+	DB.AutoMigrate(&models.Aluno{})
+}
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
